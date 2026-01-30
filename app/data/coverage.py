5058ccd82b4bc6f91b7d0bbc8df03870dcc83669
Admin <georgioji@gmail.com>
2026-01-30 13:49:55 -0700

    Added More Locations

--- app/data/coverage.py
+++ app/data/coverage.py
@@ -8,4 +8,7 @@ served_locations = [
     {"name": "Stella Condominiums (1110 11 St SW, Calgary, AB T2R 1S5)", "lat": 51.04243711958165, "lng": -114.0886222301996},
     {"name": "Cube (1177 11 Ave SW, Calgary, AB T2R 1K9)", "lat": 51.042819240606015, "lng": -114.08844428539125},
     {"name": "The Davenport (1011 12 Ave SW, Calgary, AB T2R 0J5)", "lat": 51.04187703702705, "lng": -114.08498292538731},
+    {"name": "Aya’s Appt (788 12 Ave SW, Calgary, AB T2R 0H1)", "lat": 51.04204400110349, "lng": -114.07877178701537},
+    {"name": "1200s 9 St SW, Calgary, AB T2R 1C5", "lat": 51.04169959944813, "lng": -114.0842697468219},
+    {"name": "1027 12 Ave SW, Calgary, AB T2R 0J5", "lat": 0, "lng": 0},
 ]
